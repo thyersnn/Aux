d71db8a9bc2a8196d26d91f22d7d152187688469
thyer <thyer@LAPTOP-BPQU83JP>
2017-03-17 22:36:42 -0300

    OK

---
+++ src/main/java/Auxilium/App.java
@@ -0,0 +1,13 @@
+package Auxilium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
